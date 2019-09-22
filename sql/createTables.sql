17feb30fff22ceb047a47bb044da2d4e0ec8f096
Eric <dragonoreki@gmail.com>
2019-09-22 17:06:55 -0400

    Added sql create statments and updated ERD documentation.

---
+++ sql/createTables.sql
@@ -0,0 +1,46 @@
+-- Need to add uuid-ossp extention to generate uuid's
+-- CREATE EXTENSION "uuid-ossp";
+
+CREATE TABLE users(
+   user_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+   user_account VARCHAR (255) NOT NULL,
+   account_type VARCHAR (40) NOT NULL,
+   admin_flag BOOLEAN DEFAULT 'f' NOT NULL,
+   last_login TIMESTAMP DEFAULT NOW() NOT NULL,
+   date_added TIMESTAMP DEFAULT NOW() NOT NULL,
+   date_last_updated TIMESTAMP DEFAULT NOW(),
+   active_flag BOOLEAN DEFAULT 't' NOT NULL
+);
+
+CREATE TABLE paintings(
+	painting_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    file_location VARCHAR(250) NOT NULL,
+	fractal_dimension NUMERIC(5, 3), -- Null means it needs to be calculated
+	-- Metadata for painting may be null ... be aware of this on front-end
+	name VARCHAR(150),
+	painter VARCHAR(100),
+	year_created DATE, 
+	-- End of metadata
+	date_added TIMESTAMP DEFAULT NOW() NOT NULL,
+    date_last_updated TIMESTAMP DEFAULT NOW() NOT NULL
+);
+
+CREATE TABLE user_source_files(
+   user_source_file_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+   user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE RESTRICT, -- Referential integrity constraint
+   file_location VARCHAR(250) NOT NULL,
+   fractal_dimension NUMERIC(5, 3), -- Null means it needs to be calculated
+   date_added TIMESTAMP DEFAULT NOW() NOT NULL,
+   date_last_updated TIMESTAMP DEFAULT NOW() NOT NULL
+);
+
+CREATE TABLE user_paintings(
+	user_painting_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+	painting_id UUID NOT NULL REFERENCES paintings(painting_id) ON DELETE RESTRICT,
+	user_source_file_id UUID NOT NULL REFERENCES user_source_files(user_source_file_id) ON DELETE RESTRICT,
+	user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE RESTRICT,
+	file_location VARCHAR(250) NOT NULL,
+	watermark_flag BOOLEAN DEFAULT 't' NOT NULL,
+	date_added TIMESTAMP DEFAULT NOW() NOT NULL,
+    date_last_updated TIMESTAMP DEFAULT NOW() NOT NULL
+);
